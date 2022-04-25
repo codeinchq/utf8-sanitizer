4e3db6076ef63055fb14379f604ac1e9f48baa9d
Joan Fabrégat <joan@codeinc.co>
2022-04-25 11:53:14 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# utf8-sanitizer
-A simple UTF-8 sanitizer
+# UTF-8 sanitizer
+
+A simple UTF-8 sanitizer written in PHP 8. 
+
+## Usage
+```php
+<?php
+
+$sanitizer = new \CodeInc\Utf8Sanitizer\Utf8Sanitizer();
+$sanitizer->isValidUtf8("A valid UTF-8 string."); // true
+
+$validString = $sanitizer->sanitize("An invalid UTF-8 string.");
+
+```
+
+
+## Installation
+This library is available through [Packagist](https://packagist.org/packages/codeinc/utf8-sanitizer) and can be installed using [Composer](https://getcomposer.org/):
+
+```bash
+composer require codeinc/utf8-sanitizer
+```
+
+
+## License
+This library is published under the MIT license (see the [LICENSE](LICENSE) file). 
---
+++ src/Utf8Sanitizer.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2022 Code Inc. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Visit <https://www.codeinc.co> for more information
+ */
+
+declare(strict_types=1);
+
+namespace CodeInc\Utf8Sanitizer;
+
+/**
+ * @see https://stackoverflow.com/questions/1523460/ensuring-valid-utf-8-in-php
+ * @see https://www.w3.org/International/questions/qa-forms-utf-8.en
+ */
+class Utf8Sanitizer
+{
+    public function sanitize(string $string): string
+    {
+        if (!$this->isValidUtf8($string)) {
+            $string = iconv('CP1252', 'UTF-8', $string);
+        }
+        return $string;
+    }
+
+    public function isValidUtf8(string $string): bool
+    {
+        return (bool)preg_match(
+            '%^(?:
+              [\x09\x0A\x0D\x20-\x7E]            # ASCII
+            | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+            | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+            | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+            | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+            | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+            | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+            )*$%x',
+            $string
+        );
+    }
+}
