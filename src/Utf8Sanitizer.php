1ae9699a018956496f9bdf7678c8baf7998686b3
Joan Fabrégat <joan@codeinc.co>
2022-05-06 19:37:25 +0200

    code improvement

--- src/Utf8Sanitizer.php
+++ src/Utf8Sanitizer.php
@@ -17,7 +17,7 @@ class Utf8Sanitizer
     public function sanitize(string $string): string
     {
         if (!$this->isValidUtf8($string)) {
-            $string = (string)@iconv('CP1252', 'UTF-8', $string);
+            return (string)@iconv("UTF-8", "UTF-8//IGNORE", $string);
         }
         return $string;
     }
@@ -26,14 +26,14 @@ class Utf8Sanitizer
     {
         return (bool)preg_match(
             '%^(?:
-              [\x09\x0A\x0D\x20-\x7E]            # ASCII
-            | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
-            | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
-            | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-            | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
-            | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
-            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-            | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+                  [\x09\x0A\x0D\x20-\x7E]            # ASCII
+                | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+                | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+                | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+                | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+                | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+                | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
             )*$%x',
             $string
         );
